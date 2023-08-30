47866e5d8e9f916779c932d9aee3c70f8372c58d
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-08-30 07:36:36 +0700

    Styling Components

--- 01-basic/src/app/app.component.ts
+++ 01-basic/src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // external CSS
+  // styleUrls: ['./app.component.css']
+
+  // Inline styles
+  styles: [
+    `
+      h1 {
+        color: dodgerblue;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = '01-basic';
