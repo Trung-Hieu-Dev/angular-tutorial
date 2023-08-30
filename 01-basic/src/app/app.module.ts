65baaa04b9e7e5c3be1a44471fab7904afa7230a
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-08-30 07:16:06 +0700

    Nesting Component in Component

--- 01-basic/src/app/app.module.ts
+++ 01-basic/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 // import components
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     // register all components
     AppComponent,
     ServerComponent,
+    ServersComponent,
   ],
   imports: [BrowserModule],
   providers: [],
